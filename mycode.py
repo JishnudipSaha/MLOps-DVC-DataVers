45d9df85fce7c01eab1d2210b65b86ca69ecaadb
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-20 20:28:19 +0530

    Second version of data saved

--- mycode.py
+++ mycode.py
@@ -7,9 +7,9 @@ data = {"Name": ["Jishnu", "Aritra", "Prateek"],
         "City": ["Belgharia", "Barasat", "Kota"]}
 df = pd.DataFrame(data=data)
 
-# # Adding new row to df for V2
-# new_row_loc = {"Name": "V2", "Age": 20, "City": "City1"}
-# df.loc[len(df.index)] = new_row_loc
+# Adding new row to df for V2
+new_row_loc = {"Name": "GF1", "Age": 20, "City": "City1"}
+df.loc[len(df.index)] = new_row_loc
 
 # # Adding new row to df for V3
 # new_row_loc2 = {"Name": "V3", "Age": 22, "City": "City2"}
